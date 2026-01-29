7fea2242abb1de2df84330ad5d5406ffba6200db
chris <bbhriss@gmail.com>
2026-01-28 19:34:43 -0800

    1/28 updated project id + ci/cd

--- config.py
+++ config.py
@@ -12,6 +12,6 @@ CRF = os.getenv("FFMPEG_CRF", "23")
 # testing on 1080, will change back to 720p 
 TARGET_HEIGHT = int(os.getenv("TARGET_HEIGHT", "1080"))
 
-
+PROJECT_ID = os.getenv("PROJECT_ID", "hooptuber-dev-1234")
 if not PROJECT_ID:
     raise RuntimeError("Project id is not set")
